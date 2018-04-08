f5ad419a09c937a91ee358c7e760cc17ad18d53a
JayProck <jprochnow1@gmail.com>
2018-04-08 05:26:40 -0400

    Add plate appearance parser for hits

---
+++ src/main/java/com/bitbus/fiftyeight/baseball/scrape/baseballreference/parser/HitResultParser.java
@@ -0,0 +1,86 @@
+package com.bitbus.fiftyeight.baseball.scrape.baseballreference.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import com.bitbus.fiftyeight.baseball.player.plateappearance.HitLocation;
+import com.bitbus.fiftyeight.baseball.player.plateappearance.HitType;
+import com.bitbus.fiftyeight.baseball.player.plateappearance.PlateAppearanceResult;
+import com.bitbus.fiftyeight.baseball.player.plateappearance.PlateAppearanceResultDTO;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+public class HitResultParser implements PlateAppearanceResultParser {
+
+    private List<String> startingWords;
+
+    public HitResultParser() {
+        startingWords = new ArrayList<>();
+        startingWords.add("Single");
+        startingWords.add("Double");
+        startingWords.add("Triple");
+        startingWords.add("Home Run");
+        startingWords.add("Ground-rule Double");
+    }
+
+    @Override
+    public List<String> getStartingWords() {
+        return startingWords;
+    }
+
+    @Override
+    public PlateAppearanceResultDTO parse(String resultDescription) {
+        PlateAppearanceResult result;
+        log.trace("Determining the type of hit");
+        if (resultDescription.startsWith(startingWords.get(0))) {
+            result = PlateAppearanceResult.SINGLE;
+        } else if (resultDescription.startsWith(startingWords.get(1))
+                || resultDescription.startsWith(startingWords.get(4))) {
+            result = PlateAppearanceResult.DOUBLE;
+        } else if (resultDescription.startsWith(startingWords.get(2))) {
+            result = PlateAppearanceResult.TRIPLE;
+        } else if (resultDescription.startsWith(startingWords.get(3))) {
+            result = PlateAppearanceResult.HOMERUN;
+        } else {
+            throw new RuntimeException(
+                    "The result description is not a hit and should not be parsed by the HitResultParser");
+        }
+        log.trace("Found hit type {}", result);
+
+        log.trace("Determining the hit details: location and hit type");
+        String[] hitDescriptionParts = resultDescription.split("\\(|\\)");
+        String[] summaryParts = hitDescriptionParts[0].split("\\sto\\s");
+        String[] hitDetailParts = hitDescriptionParts[1].split("\\sto\\s|\\sthru\\s");
+        HitType hitType = HitType.findByDisplayName(hitDetailParts[0]);
+        log.trace("Hit type: {}", hitType);
+        HitLocation hitLocation;
+        if (hitDetailParts.length > 1) {
+            hitLocation = HitLocation.findByDisplayName(hitDetailParts[1]);
+        } else {
+            hitLocation = HitLocation.findByDisplayName(summaryParts[1]);
+        }
+        log.trace("Hit location: {}", hitLocation);
+
+        // TODO - Are there any error scenarios that need to be accounted for? E.g. runner scores on
+        // bad throw?
+        log.trace("Determining RBIs");
+        int runnersScored = StringUtils.countMatches(resultDescription, "Scores");
+        int runsBattedIn = (result == PlateAppearanceResult.HOMERUN) ? runnersScored + 1 : runnersScored;
+        log.trace("RBIs: {}", runsBattedIn);
+
+        log.debug("Successfully parsed the hit result data");
+        return PlateAppearanceResultDTO.builder(result) //
+                .hit(true) //
+                .qualifiedAtBat(true) //
+                .hitType(hitType) //
+                .hitLocation(hitLocation) //
+                .runsBattedIn(runsBattedIn) //
+                .build();
+    }
+
+}
---
+++ src/test/java/com/bitbus/fiftyeight/baseball/scrape/baseballreference/parser/HitResultParserTest.java
@@ -0,0 +1,144 @@
+package com.bitbus.fiftyeight.baseball.scrape.baseballreference.parser;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.bitbus.fiftyeight.baseball.player.plateappearance.HitLocation;
+import com.bitbus.fiftyeight.baseball.player.plateappearance.HitType;
+import com.bitbus.fiftyeight.baseball.player.plateappearance.PlateAppearanceResult;
+import com.bitbus.fiftyeight.baseball.player.plateappearance.PlateAppearanceResultDTO;
+
+public class HitResultParserTest {
+
+    HitResultParser parser = new HitResultParser();
+
+    @Test
+    public void testNotParsableBy() {
+        assertFalse(parser.isParserFor("Groundout: 3B-1B (Weak 3B)"));
+        assertFalse(parser.isParserFor("Walk"));
+        assertFalse(parser.isParserFor("Strikeout Swinging"));
+        assertFalse(parser.isParserFor("Flyball: LF/Sacrifice Fly (Deep LF); Gordon Scores/unER"));
+        assertFalse(parser.isParserFor("Lineout: LF (Deep LF-CF)"));
+        assertFalse(parser.isParserFor("Popfly: 2B (Deep SS-2B)"));
+        assertFalse(parser.isParserFor("Foul Flyball: LF (LF into Foul Terr.)"));
+        assertFalse(parser.isParserFor("Wild Pitch; Cespedes to 2B"));
+    }
+
+    @Test
+    public void testParsableBy() {
+        assertTrue(parser.isParserFor("Single to 3B (Ground Ball to SS-3B Hole); Ellis to 2B"));
+        assertTrue(parser.isParserFor("Double to LF (Line Drive to Deep LF Line); Ellis Scores/unER; Yelich to 3B"));
+        assertTrue(parser.isParserFor("Triple to RF (Ground Ball); Flores Scores; Bruce Scores; Walker Scores"));
+        assertTrue(parser
+                .isParserFor("Home Run (Line Drive to Deep LF Line); Ellis Scores; Yelich Scores; Stanton Scores"));
+        assertTrue(parser.isParserFor("Ground-rule Double (Line Drive to Deep CF)"));
+    }
+
+    @Test
+    public void testParseSingle() {
+        PlateAppearanceResultDTO dto = parser.parse("Single to LF (Ground Ball)");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.SINGLE, dto.getResult());
+        assertEquals(HitType.GROUND_BALL, dto.getHitType());
+        assertEquals(HitLocation.LEFT_FIELD, dto.getHitLocation());
+        assertEquals(0, dto.getRunsBattedIn());
+
+        dto = parser.parse("Single to CF (Ground Ball thru SS-2B); Owings Scores; Goldschmidt Scores");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.SINGLE, dto.getResult());
+        assertEquals(HitType.GROUND_BALL, dto.getHitType());
+        assertEquals(HitLocation.SHORT_STOP_SECOND_BASE, dto.getHitLocation());
+        assertEquals(2, dto.getRunsBattedIn());
+
+        dto = parser.parse("Single to LF (Line Drive to Short LF)");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.SINGLE, dto.getResult());
+        assertEquals(HitType.LINE_DRIVE, dto.getHitType());
+        assertEquals(HitLocation.SHORT_LEFT_FIELD, dto.getHitLocation());
+        assertEquals(0, dto.getRunsBattedIn());
+    }
+
+    @Test
+    public void testParseDouble() {
+        PlateAppearanceResultDTO dto = parser.parse("Double to RF (Fly Ball to Deep CF-RF)");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.DOUBLE, dto.getResult());
+        assertEquals(HitType.FLYBALL, dto.getHitType());
+        assertEquals(HitLocation.DEEP_CENTER_FIELD_RIGHT_FIELD, dto.getHitLocation());
+        assertEquals(0, dto.getRunsBattedIn());
+
+        dto = parser.parse("Double to RF (Line Drive to RF Line)");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.DOUBLE, dto.getResult());
+        assertEquals(HitType.LINE_DRIVE, dto.getHitType());
+        assertEquals(HitLocation.RIGHT_FIELD_LINE, dto.getHitLocation());
+        assertEquals(0, dto.getRunsBattedIn());
+
+        dto = parser.parse("Double to LF (Line Drive); Owings Scores; Goldschmidt Scores; Lamb to 3B");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.DOUBLE, dto.getResult());
+        assertEquals(HitType.LINE_DRIVE, dto.getHitType());
+        assertEquals(HitLocation.LEFT_FIELD, dto.getHitLocation());
+        assertEquals(2, dto.getRunsBattedIn());
+    }
+
+    @Test
+    public void testParseTriple() {
+        PlateAppearanceResultDTO dto =
+                parser.parse("Triple to RF (Ground Ball); Flores Scores; Bruce Scores; Walker Scores");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.TRIPLE, dto.getResult());
+        assertEquals(HitType.GROUND_BALL, dto.getHitType());
+        assertEquals(HitLocation.RIGHT_FIELD, dto.getHitLocation());
+        assertEquals(3, dto.getRunsBattedIn());
+    }
+
+    @Test
+    public void testParseHomeRun() {
+        PlateAppearanceResultDTO dto = parser.parse("Home Run (Fly Ball to Deep CF); Pederson Scores");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.HOMERUN, dto.getResult());
+        assertEquals(HitType.FLYBALL, dto.getHitType());
+        assertEquals(HitLocation.DEEP_CENTER_FIELD, dto.getHitLocation());
+        assertEquals(2, dto.getRunsBattedIn());
+
+        dto = parser.parse("Home Run (Line Drive to Deep LF Line); Ellis Scores; Yelich Scores; Stanton Scores");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.HOMERUN, dto.getResult());
+        assertEquals(HitType.LINE_DRIVE, dto.getHitType());
+        assertEquals(HitLocation.DEEP_LEFT_FIELD_LINE, dto.getHitLocation());
+        assertEquals(4, dto.getRunsBattedIn());
+
+        dto = parser.parse("Home Run (Fly Ball to Deep LF)");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.HOMERUN, dto.getResult());
+        assertEquals(HitType.FLYBALL, dto.getHitType());
+        assertEquals(HitLocation.DEEP_LEFT_FIELD, dto.getHitLocation());
+        assertEquals(1, dto.getRunsBattedIn());
+    }
+
+    @Test
+    public void testParseGroundRuleDouble() {
+        PlateAppearanceResultDTO dto = parser.parse("Ground-rule Double (Line Drive to Deep CF)");
+        assertTrue(dto.isHit());
+        assertTrue(dto.isQualifiedAtBat());
+        assertEquals(PlateAppearanceResult.DOUBLE, dto.getResult());
+        assertEquals(HitType.LINE_DRIVE, dto.getHitType());
+        assertEquals(HitLocation.DEEP_CENTER_FIELD, dto.getHitLocation());
+        assertEquals(0, dto.getRunsBattedIn());
+    }
+
+}
