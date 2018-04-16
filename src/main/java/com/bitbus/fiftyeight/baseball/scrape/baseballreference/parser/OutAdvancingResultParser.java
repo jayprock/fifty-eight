8efacf5bbe7461f3c1303b6ebb9354ba548e3c8f
JayProck <jprochnow1@gmail.com>
2018-04-16 06:11:38 -0400

    Add parser for base runner out advancing

---
+++ src/main/java/com/bitbus/fiftyeight/baseball/scrape/baseballreference/parser/OutAdvancingResultParser.java
@@ -0,0 +1,31 @@
+
+package com.bitbus.fiftyeight.baseball.scrape.baseballreference.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.bitbus.fiftyeight.baseball.player.plateappearance.PlateAppearanceResultDTO;
+
+@Component
+public class OutAdvancingResultParser implements PlateAppearanceResultParser {
+
+    private List<String> startingWords;
+
+    public OutAdvancingResultParser() {
+        startingWords = new ArrayList<>();
+        startingWords.add("Baserunner Out Advancing");
+    }
+
+    @Override
+    public List<String> getStartingWords() {
+        return startingWords;
+    }
+
+    @Override
+    public PlateAppearanceResultDTO parse(String resultDescription) {
+        return PlateAppearanceResultDTO.builder(null).notPlateAppearanceResult(true).build();
+    }
+
+}
