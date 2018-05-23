3614998d51979af6445a2a59454be76bd90ad3bc
JayProck <jprochnow1@gmail.com>
2018-05-23 06:15:15 -0400

    Allow up to 5 selenium exceptions during the parsing

--- src/main/java/com/bitbus/fiftyeight/baseball/scrape/baseballreference/BaseballReferenceScrapeMain.java
+++ src/main/java/com/bitbus/fiftyeight/baseball/scrape/baseballreference/BaseballReferenceScrapeMain.java
@@ -1,8 +1,11 @@
 package com.bitbus.fiftyeight.baseball.scrape.baseballreference;
 
+import static com.bitbus.fiftyeight.common.scrape.ScrapeSleeper.sleep;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -39,41 +42,51 @@ public class BaseballReferenceScrapeMain {
 
     private void performScrape() {
         List<ScrapeResult> errorResults = new ArrayList<>();
-        try {
-            String mainTabHandle = scraper.openPageWithBoxscores();
-            log.info("Looking up the per day game blocks");
-            List<WebElement> dailyBoxscoreBlocks = scraper.getDailyBoxscoreBlocks();
-            for (WebElement dailyBoxscoreBlock : dailyBoxscoreBlocks) {
-                List<WebElement> boxscoreLinks = scraper.getBoxscoreLinks(dailyBoxscoreBlock);
-                for (WebElement boxscoreLink : boxscoreLinks) {
-                    String matchupBaseballReferenceId =
-                            baseballReferenceIdParser.parse(boxscoreLink.getAttribute("href"));
-                    if (matchupService.matchupExistsForBaseballReferenceId(matchupBaseballReferenceId)) {
-                        log.info("Matchup with ID {} has already been processed. Skipping.",
-                                matchupBaseballReferenceId);
-                        continue;
-                    }
-                    try {
-                        scraper.processBoxScore(boxscoreLink, mainTabHandle, matchupBaseballReferenceId);
-                    } catch (IllegalArgumentException | ScrapeException e) {
-                        Result result;
-                        if (e instanceof WarningScrapeException) {
-                            result = Result.WARNING;
-                        } else {
-                            result = Result.FAILURE;
+        int seleniumErrorCount = 0;
+        boolean done = false;
+        while (!done && seleniumErrorCount < 5) {
+            try {
+                String mainTabHandle = scraper.openPageWithBoxscores();
+                log.info("Looking up the per day game blocks");
+                List<WebElement> dailyBoxscoreBlocks = scraper.getDailyBoxscoreBlocks();
+                for (WebElement dailyBoxscoreBlock : dailyBoxscoreBlocks) {
+                    List<WebElement> boxscoreLinks = scraper.getBoxscoreLinks(dailyBoxscoreBlock);
+                    for (WebElement boxscoreLink : boxscoreLinks) {
+                        String matchupBaseballReferenceId =
+                                baseballReferenceIdParser.parse(boxscoreLink.getAttribute("href"));
+                        if (matchupService.matchupExistsForBaseballReferenceId(matchupBaseballReferenceId)) {
+                            log.info("Matchup with ID {} has already been processed. Skipping.",
+                                    matchupBaseballReferenceId);
+                            continue;
                         }
-                        String message = result + ": " + e.getMessage();
-                        errorResults.add(new ScrapeResult(result, message));
+                        try {
+                            scraper.processBoxScore(boxscoreLink, mainTabHandle, matchupBaseballReferenceId);
+                        } catch (IllegalArgumentException | ScrapeException e) {
+                            Result result;
+                            if (e instanceof WarningScrapeException) {
+                                result = Result.WARNING;
+                            } else {
+                                result = Result.FAILURE;
+                            }
+                            String message = result + ": " + e.getMessage();
+                            errorResults.add(new ScrapeResult(result, message));
+                        }
                     }
                 }
+                done = true;
+            } catch (WebDriverException e) {
+                log.error("Selenium WebDriverException during parsing", e);
+                seleniumErrorCount++;
+                log.info("Sleeping for 2 minutes to hopefully recover from Selenium error");
+                sleep(2 * 60 * 1000);
+            } catch (Exception e) {
+                log.error("An error occurred during the baseball reference scrape", e);
+                done = true;
             }
-        } catch (Exception e) {
-            log.error("An error occurred during the baseball reference scrape", e);
-            log.warn("Identified {} warnings or errors during the scrape process", errorResults.size());
-            for (ScrapeResult errorResult : errorResults) {
-                System.out.println(errorResult.getMessage());
-            }
-
+        }
+        log.warn("Identified {} warnings or errors during the scrape process", errorResults.size());
+        for (ScrapeResult errorResult : errorResults) {
+            System.out.println(errorResult.getMessage());
         }
     }
 
